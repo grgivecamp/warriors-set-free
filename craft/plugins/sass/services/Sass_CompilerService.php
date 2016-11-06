1192e64414cbbae4a548b50278fd06a8d596f2e9
Bob Orchard <bob@webnomads.io>
2016-11-06 04:51:55 -0500

    Update Plugin: SASS

--- craft/plugins/sass/services/Sass_CompilerService.php
+++ craft/plugins/sass/services/Sass_CompilerService.php
@@ -7,36 +7,46 @@
 
 namespace Craft;
 
-use Exception;
-use scssc;
+use Leafo\ScssPhp\Server;
 
 /**
- * Sass_CompilerService is a Craft plugin service that exposes the sass
+ * Sass_Compileview-source:rService is a Craft plugin service that exposes the sass
  * compiler to Sass_CompilerController.
  */
 class Sass_CompilerService extends BaseApplicationComponent
 {
     /**
-     * compiler is an instance of scssc;
+     * compiler is an instance of Leafo\ScssPhp\Server;
      *
-     * @var scssc
+     * @var server
      */
-    protected $compiler = null;
+    private static $scss_server = null;
 
     /**
-     * getCompiler() reurns and instance of scssc, constructing one if
+     * server() returns an instance of Leafo\ScssPhp\Server, constructing it if
      * necessary.
      *
-     * @return scssc
+     * If you provide an instance of Server as argument, it uses that instead.
+     *
+     * @return Server
      */
-    public function getCompiler()
+    public static function server($instance = null)
     {
-        if ($this->compiler === null) {
-            include dirname(__DIR__) . '/vendor/autoload.php';
-            $this->compiler = new scssc();
+        if ($instance !== null) {
+            static::$scss_server = $instance;
+        } elseif (static::$scss_server === null) {
+            $sass_cache_dir = craft()->path->getStoragePath() . '/scss_cache';
+            IOHelper::ensureFolderExists($sass_cache_dir);
+
+            static::$scss_server = new Server(
+                dirname(craft()->request->getScriptFile()),
+                $sass_cache_dir
+            );
+
+            static::$scss_server->showErrorsAsCSS();
         }
 
-        return $this->compiler;
+        return static::$scss_server;
     }
 
     /**
@@ -47,35 +57,13 @@ class Sass_CompilerService extends BaseApplicationComponent
      */
     public function compile($filename)
     {
-        $document_root = dirname(craft()->request->getScriptFile());
+        $_GET['p'] = $filename;
 
-        $filename = realpath("$document_root/$filename");
+        ob_start();
+        static::server()->serve();
+        $css = ob_get_contents();
+        ob_end_clean();
 
-        if (file_exists($filename) && strpos($filename, $document_root) === 0) {
-            $scss = file_get_contents($filename);
-
-            $compiler = $this->getCompiler();
-            $compiler->setImportPaths($document_root);
-
-            try {
-
-                return $compiler->compile($scss);
-
-            } catch (Exception $e) {
-                $msg = $e->getFile() . ":\n\n";
-                $msg .= $e->getMessage();
-                $msg = str_replace(array("'", "\n"), array("\\'", "\\A"), $msg);
-                return "body { display: none !important; }
-                    html:after {
-                        background: white;
-                        color: black;
-                        content: '$msg';
-                        display: block !important;
-                        font-family: Mono;
-                        padding: 1em;
-                        white-space: pre;
-                    }";
-            }
-        }
+        return $css;
     }
 }
